5251bea0195b937e90a56341bb10f897fb8693ee
Paz Spera <mspera03@gmail.com>
2021-10-21 00:51:50 -0300

    modifiqué navbar para que el movimiento entre secciones se haga mediante jQuery

--- js/main.js
+++ js/main.js
@@ -429,3 +429,21 @@ const eliminarProductoDePedido = (id) => {
     activarIndicadorCart();
 }
 
+// Animación entre secciones del sitio
+$('#navbar-anchor').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('#navbar').offset().top
+    }, 1500);
+});
+
+$('#navbar-footer').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('#footer').offset().top
+    }, 1500);
+});
+
+$('#navbar-menu').on('click', function(){
+    $('html, body').animate({
+        scrollTop: $('#anchor_menu').offset().top
+    }, 1500);
+});
