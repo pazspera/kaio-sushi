32b238572a691a709bae3a0075e40ca7c981b859
Paz Spera <mspera03@gmail.com>
2021-10-22 00:27:28 -0300

    moví los productos a un json y cambié el código de productos.js para que las cards se impriman mediante una petición get al json

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 /* 
-    DESAFÍO CLASE 12 - Incorporar jQuery al proyecto
+    AGREGAR EL MENÚ CON MI PEDIDO 
 
-    -   Reescribí la función imprimirCards() (línea 43) para imprimir en el DOM con jQuery
-    -   Reescribí la función para hacer el toggle de cards dependiendo de si son piezas o combos con jQuery (línea 121)
+    Al tocar el ícono de cart, desde la derecha aparece
+    un panel con el resultado de pedidoArray. 
+    Usar Offcanvas de Bootstrap para esto
 
-    -   Actualicé el funcionamiento del carrito para que se muestre el total de items agregado de cada producto 
-        en vez de escribir cada producto individualmente. Hice los cambios en agregarProductoADetallePedido(), 
-        eliminarProductoDePedido(), calcularTotalPedido() y calcularTiempoPreparacionPedido()
+    INCORPORAR AJAX
+
+
+    DIVIDIR ARCHIVO MAIN.JS EN VARIOS ARCHIVOS
+
+
+    AGREGAR FORMULARIO PARA PAGAR 
+
+
+    AGREGAR HTML CON CONFIRMACION DE PEDIDO
     
 */
 
@@ -29,150 +37,13 @@ let $totalPedido = document.createElement('p');
 let $costoEnvio = document.createElement('p');
 let $totalAPagar = document.createElement('p');
 let $tiempoPreparacion = document.createElement('p');
-let $cardsMenu;
+
 let $cartIndicador = document.querySelector('.cart__indicador');
 let $cart = document.querySelector('.cart__icon');
-let $menuPiezas = document.getElementById('menu-piezas');
-let $menuCombos = document.getElementById('menu-combos');
-let $menuOpciones = document.getElementById('menu-opciones');
+
 
 
 // --------- FUNCIONES ---------
-// Imprimir cards de producto en #menu-cards
-// Función con jQuery
-const imprimirCards = (array) => {
-    for (item of array){
-        $('#menu-cards').append(`
-        <div class="col">
-            <div class="card h-100" id="${item.id}">
-                <img src="./img/img_${item.id}.jpg" class="card-img-top" alt="${item.descripcion}">
-                <div class="card-body">
-                <div class="mb-2">
-                    <h3 class="card__title">${item.nombre}</h3>
-                    <p class="card__description">${item.descripcion}</p>
-                </div>
-                <div class="card__bottom-info">
-                    <div class="card__price mb-3">
-                        <p class="card__price__text">${item.piezas} piezas</p>
-                        <p class="card__price__amount">$${item.precio}</p>
-                    </div>
-                    <button class="btn btn__secondary btn__card-pedido">Agregar a pedido</button>
-                </div>  
-                </div>
-            </div>
-        </div>`)
-    }
-}
-// Función con Vanilla JS
-/* const imprimirCards = (array) => { 
-    for(item of array){
-        let $card = document.createElement('div');
-        $card.classList.add('col');
-        $card.innerHTML = `
-            <div class="card h-100" id="${item.id}">
-                <img src="./img/img_${item.id}.jpg" class="card-img-top" alt="${item.descripcion}">
-                <div class="card-body">
-                    <div class="mb-2">
-                        <h3 class="card__title">${item.nombre}</h3>
-                        <p class="card__description">${item.descripcion}</p>
-                    </div>
-                    <div class="card__bottom-info">
-                        <div class="card__price mb-3">
-                            <p class="card__price__text">${item.piezas} piezas</p>
-                            <p class="card__price__amount">$${item.precio}</p>
-                        </div>
-                        <button class="btn btn__secondary btn__card-pedido">Agregar a pedido</button>
-                    </div>  
-                </div>
-            </div>
-        </div>`;
-        $menuCards.appendChild($card);
-    }
-} */
-
-
-// Función para agregar event listners a btn de cards
-const agregarEventosBtnCards = (e)=>{
-    pedidoId = 0;
-    $cardsMenu = document.querySelectorAll('.card');
-    // Agrega eventListener a cada card
-    $cardsMenu.forEach(card => {
-        card.addEventListener('click', (e) =>{
-            // Hace que el eventListener se active solo al clickear el btn de agregar pedido
-            if(e.target.matches('.btn__card-pedido')){
-                // Obtiene objeto producto en base al id
-                let objetoProducto = buscarObjetoPorId(card.id);
-                // Hago una copia del objeto a agregar con JSON.parse
-                // y JSON.stringify para asegurar que cada producto
-                // del array tenga su propio id
-                let copiaObjetoProducto = JSON.parse(JSON.stringify(objetoProducto));
-                agregarProductoAPedido(copiaObjetoProducto);
-                calcularTotalPedido(pedidoArray);
-                mostrarEstadoPedido();
-            }
-        })
-    });
-}
-
-
-// Menú: muestra y oculta cards dependiendo si el btn piezas o combos está activado
-// Función con jQuery
-$('#menu-opciones').on('click', (e) => {
-    if(e.target.matches('#menu-piezas')){
-        // Toggle estado activo de los btn 
-        $('#menu-piezas').addClass( "menu-active" );
-        $('#menu-combos').removeClass( "menu-active" );
-        // Toggle estado activo de los btn
-        $($menuCards).empty();
-        // Imprime cards de menuPiezas
-        imprimirCards(menuPiezas);
-    }
-    
-    if(e.target.matches('#menu-combos')){
-        console.log('combos');
-        // Toggle estado activo de los btn
-        $('#menu-combos').addClass('menu-active');
-        $('#menu-piezas').removeClass('menu-active');
-        // Borra elementos ya impresos
-        $($menuCards).empty();
-        // Imprime cards de menuCombos
-        imprimirCards(menuCombos);
-    }
-
-    agregarEventosBtnCards();
-})
-// Función con Vanilla JS
-/* $menuOpciones.addEventListener('click', (e) =>{
-    if(e.target.matches('#menu-piezas')){
-        // Toggle estado activo de los btn 
-        $menuPiezas.classList.add('menu-active');
-        $menuCombos.classList.remove('menu-active');
-        // Toggle estado activo de los btn
-        $($menuCards).empty();
-        // Imprime cards de menuPiezas
-        imprimirCards(menuPiezas);
-    }
-    
-    if(e.target.matches('#menu-combos')){
-        // Toggle estado activo de los btn
-        $menuCombos.classList.add('menu-active');
-        $menuPiezas.classList.remove('menu-active');
-        // Borra elementos ya impresos
-        $($menuCards).empty();
-        // Imprime cards de menuCombos
-        imprimirCards(menuCombos);
-    }
-
-    agregarEventosBtnCards();
-}); */
-
-
-// Por defecto, el menú de piezas va a estar seleccionado
-// Cuando se carga la página, se cargan automáticamente las cards de menuPiezas
-window.addEventListener('load', (e)=>{
-    imprimirCards(menuPiezas);
-    agregarEventosBtnCards();
-});
 
 
 // Buscar la info de producto en productos[] con el id de la card
--- js/productos.js
+++ js/productos.js
@@ -1,47 +1,6 @@
-const productos = [
-    {
-        id: 'roll_new_york',
-        nombre: 'New York',
-        precio: 350,
-        piezas: 5,
-        tipo: 'piezas',
-        descripcion: 'Salmón rosado, queso Philadelphia y palta.',
-    },
-    {
-        id: 'roll_atun',
-        nombre: 'Atún',
-        precio: 250,
-        piezas: 5,
-        tipo: 'piezas',
-        descripcion: 'Atún, queso Philadelphia y palta.',
-    },
-    {
-        id: 'roll_salmon_doble',
-        nombre: 'Salmón doble',
-        precio: 450,
-        piezas: 5,
-        tipo: 'piezas',
-        descripcion: 'Relleno de salmón y palta con cobertura de salmón.',
-    },
-    {
-        id: 'sashimi',
-        nombre: 'Sashimi',
-        precio: 450,
-        piezas: 5,
-        tipo: 'piezas',
-        descripcion: 'Lonjas de salmón rosado.',
-    },
-    {
-        id: 'promo_20_piezas',
-        nombre: 'Promo 20 piezas',
-        precio: 1900,
-        piezas: 20,
-        tipo: 'combo',
-        descripcion: '5 rolls Nueva York, 5 rolls de atún, 5 rolls de salmón doble y 5 sashimi.',
-    },
-]
+const URL_JSON = "json/productos.json";
 
-// Guardar array con todos los productos en localStorage
+/* // Guardar array con todos los productos en localStorage
 let productosJSON = JSON.stringify(productos)
 localStorage.setItem('productos', productosJSON);
 
@@ -55,5 +14,170 @@ let menuProductos = JSON.parse(localStorage.getItem('productos'));
 for(producto of menuProductos){
     if(producto.tipo === 'combo') menuCombos.push(producto);
     if(producto.tipo === 'piezas') menuPiezas.push(producto);
+} */
+
+
+const menuPiezas = [];
+const menuCombos = [];
+
+let $cardsMenu;
+let $menuPiezas = document.getElementById('menu-piezas');
+let $menuCombos = document.getElementById('menu-combos');
+let $menuOpciones = document.getElementById('menu-opciones');
+
+// Recuperar los productos desde productos.json
+$(document).ready( () => {
+    $.get(URL_JSON, function(respuesta, estado){
+        console.log(respuesta);
+        console.log(estado);
+        if(estado === 'success'){
+            let productos = respuesta;
+            // Divide los productos en dos arrays: combos y piezas
+            for(producto of productos){
+                if(producto.tipo === 'combo') menuCombos.push(producto);
+                if(producto.tipo === 'piezas') menuPiezas.push(producto);
+            }
+            imprimirCards(menuPiezas);
+            agregarEventosBtnCards();
+            console.log(menuPiezas);
+            console.log(menuCombos);
+        }
+    })
+});
+
+// Imprimir cards de producto en #menu-cards
+// Función con jQuery
+/* const imprimirCards = (array) => {
+    for (item of array){
+        $('#menu-cards').append(`
+        <div class="col">
+            <div class="card h-100" id="${item.id}">
+                <img src="./img/img_${item.id}.jpg" class="card-img-top" alt="${item.descripcion}">
+                <div class="card-body">
+                <div class="mb-2">
+                    <h3 class="card__title">${item.nombre}</h3>
+                    <p class="card__description">${item.descripcion}</p>
+                </div>
+                <div class="card__bottom-info">
+                    <div class="card__price mb-3">
+                        <p class="card__price__text">${item.piezas} piezas</p>
+                        <p class="card__price__amount">$${item.precio}</p>
+                    </div>
+                    <button class="btn btn__secondary btn__card-pedido">Agregar a pedido</button>
+                </div>  
+                </div>
+            </div>
+        </div>`)
+    }
+} */
+// Función con Vanilla JS
+const imprimirCards = (array) => { 
+    for(item of array){
+        let $card = document.createElement('div');
+        $card.classList.add('col');
+        $card.innerHTML = `
+            <div class="card h-100" id="${item.id}">
+                <img src="./img/img_${item.id}.jpg" class="card-img-top" alt="${item.descripcion}">
+                <div class="card-body">
+                    <div class="mb-2">
+                        <h3 class="card__title">${item.nombre}</h3>
+                        <p class="card__description">${item.descripcion}</p>
+                    </div>
+                    <div class="card__bottom-info">
+                        <div class="card__price mb-3">
+                            <p class="card__price__text">${item.piezas} piezas</p>
+                            <p class="card__price__amount">$${item.precio}</p>
+                        </div>
+                        <button class="btn btn__secondary btn__card-pedido">Agregar a pedido</button>
+                    </div>  
+                </div>
+            </div>
+        </div>`;
+        $menuCards.appendChild($card);
+    }
 }
 
+
+// Función para agregar event listners a btn de cards
+const agregarEventosBtnCards = (e)=>{
+    pedidoId = 0;
+    $cardsMenu = document.querySelectorAll('.card');
+    // Agrega eventListener a cada card
+    $cardsMenu.forEach(card => {
+        card.addEventListener('click', (e) =>{
+            // Hace que el eventListener se active solo al clickear el btn de agregar pedido
+            if(e.target.matches('.btn__card-pedido')){
+                // Obtiene objeto producto en base al id
+                let objetoProducto = buscarObjetoPorId(card.id);
+                // Hago una copia del objeto a agregar con JSON.parse
+                // y JSON.stringify para asegurar que cada producto
+                // del array tenga su propio id
+                let copiaObjetoProducto = JSON.parse(JSON.stringify(objetoProducto));
+                agregarProductoAPedido(copiaObjetoProducto);
+                calcularTotalPedido(pedidoArray);
+                mostrarEstadoPedido();
+            }
+        })
+    });
+}
+
+
+// Menú: muestra y oculta cards dependiendo si el btn piezas o combos está activado
+// Función con jQuery
+/* $('#menu-opciones').on('click', (e) => {
+    if(e.target.matches('#menu-piezas')){
+        // Toggle estado activo de los btn 
+        $('#menu-piezas').addClass( "menu-active" );
+        $('#menu-combos').removeClass( "menu-active" );
+        // Toggle estado activo de los btn
+        $($menuCards).empty();
+        // Imprime cards de menuPiezas
+        imprimirCards(menuPiezas);
+    }
+    
+    if(e.target.matches('#menu-combos')){
+        console.log('combos');
+        // Toggle estado activo de los btn
+        $('#menu-combos').addClass('menu-active');
+        $('#menu-piezas').removeClass('menu-active');
+        // Borra elementos ya impresos
+        $($menuCards).empty();
+        // Imprime cards de menuCombos
+        imprimirCards(menuCombos);
+    }
+
+    agregarEventosBtnCards();
+}) */
+
+// Función con Vanilla JS
+$menuOpciones.addEventListener('click', (e) =>{
+    if(e.target.matches('#menu-piezas')){
+        // Toggle estado activo de los btn 
+        $menuPiezas.classList.add('menu-active');
+        $menuCombos.classList.remove('menu-active');
+        // Toggle estado activo de los btn
+        $($menuCards).empty();
+        // Imprime cards de menuPiezas
+        imprimirCards(menuPiezas);
+    }
+    
+    if(e.target.matches('#menu-combos')){
+        // Toggle estado activo de los btn
+        $menuCombos.classList.add('menu-active');
+        $menuPiezas.classList.remove('menu-active');
+        // Borra elementos ya impresos
+        $($menuCards).empty();
+        // Imprime cards de menuCombos
+        imprimirCards(menuCombos);
+    }
+
+    agregarEventosBtnCards();
+});
+
+
+// Por defecto, el menú de piezas va a estar seleccionado
+// Cuando se carga la página, se cargan automáticamente las cards de menuPiezas
+/* window.addEventListener('load', (e)=>{
+    imprimirCards(menuPiezas);
+    agregarEventosBtnCards();
+}); */
