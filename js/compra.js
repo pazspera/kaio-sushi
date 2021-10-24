b69155d4d3999db021c1e02d5bf392a05f3c366e
Paz Spera <mspera03@gmail.com>
2021-10-24 01:24:36 -0300

    agregué compra.html para que, al hacer click en el btn comprar del offcanvas, vaya a la página de compra

---
+++ js/compra.js
@@ -0,0 +1,17 @@
+/* 
+    Al hacer click en el btn-comprar, guardar el pedido en local storage
+    Usar la info del local storage para imprimir el detalle de pedido en la parte izquierda de compra.html
+    En la parte derecha de compra.html, tener el formulario con los datos a completar:
+    - Nombre
+    - Email
+    - Dirección
+    - Teléfono
+
+    Info tarjeta: 
+    - Número de tarjeta
+    - Nombre Titular
+    - Desde
+    - Hasta
+    - Cvc
+    
+*/
